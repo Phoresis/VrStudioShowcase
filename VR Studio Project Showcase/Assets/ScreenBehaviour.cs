ddad29060cb0955859b87bfef49f08a77c7a0e96
White, Richard <richard.j.white@avanade.com>
2018-11-16 14:07:49 -0500

    Work on screen.

--- VR Studio Project Showcase/Assets/ScreenBehaviour.cs
+++ VR Studio Project Showcase/Assets/ScreenBehaviour.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(VideoPlayer))]
 public class ScreenBehaviour : MonoBehaviour {
 
     public Texture2D Thumbnail;
@@ -11,34 +12,70 @@ public class ScreenBehaviour : MonoBehaviour {
 
     private bool _isGazedAt;
     private float _elapsedSeconds;
+    private Renderer _renderer;
+    private VideoPlayer _videoPlayer;
 
     void Start()
     {
         _isGazedAt = false;
-        GetComponent<Renderer>().material.mainTexture = Thumbnail;
+        _renderer = GetComponent<Renderer>();
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _renderer.material.mainTexture = Thumbnail;
+    }
+
+    private IEnumerator PlayVideo()
+    {
+        if (_videoPlayer.isPlaying)
+        {
+            yield return null;
+        }
+
+        _videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!_videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+        _renderer.material.mainTexture = _videoPlayer.texture;
+
+        _videoPlayer.Play();
+    }
+
+    private void StopVideo()
+    {
+        if (_videoPlayer.isPlaying)
+        {
+            _renderer.material.mainTexture = Thumbnail;
+            _videoPlayer.Stop();
+        }
     }
 
     // Update is called once per frame
-    void Update () {
-		
-        if (_elapsedSeconds >= TimeToVideo)
+    void Update () {       
+
+        if (_isGazedAt && _elapsedSeconds >= TimeToVideo)
         {
-            GetComponent<Renderer>().material.mainTexture = null;
+            StartCoroutine(PlayVideo());
+            _elapsedSeconds = 0;
         }
 
-        if (_isGazedAt)
+        if (!_isGazedAt && _elapsedSeconds >= TimeBackToThumbnail)
         {
-            _elapsedSeconds += Time.deltaTime;
+            StopVideo();
+            _elapsedSeconds = 0;
         }
+
+        _elapsedSeconds += Time.deltaTime;
 	}
 
     public void SetGazedAt(bool gazedAt)
     {
         _isGazedAt = gazedAt;
-
-        if (!gazedAt)
-        {
-            _elapsedSeconds = 0;
-        }
+        _elapsedSeconds = 0;
     }
 }
